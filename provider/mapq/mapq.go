54d9b1d9f3d64b4c0f0729c8a32fee1e4dfc5e2b
savaki <matt.ho@gmail.com>
2015-07-19 23:58:58 -0700

    - moved code into a single file for easier comprehension

--- provider/mapq/mapq.go
+++ provider/mapq/mapq.go
@@ -1,8 +1,55 @@
 package mapq
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/savaki/graphql"
+)
 
 var (
 	errNotImplemented = errors.New("not implemented")
 	errFieldNotFound  = errors.New("field not found")
 )
+
+// --[ Field ]------------------------------------------------------------
+
+type field struct {
+	value interface{}
+}
+
+func (f *field) Value() (graphql.Value, error) {
+	return f.value, nil
+}
+
+func (f *field) Selection() (graphql.Selection, error) {
+	switch v := f.value.(type) {
+	case map[string]interface{}:
+		return &selection{data: v}, nil
+	}
+
+	return nil, errNotImplemented
+}
+
+// --[ Store / Selection ]------------------------------------------------
+
+type selection struct {
+	data map[string]interface{}
+}
+
+func New(data map[string]interface{}) graphql.Store {
+	return &selection{
+		data: data,
+	}
+}
+
+func (s *selection) Query(c *graphql.Context) (graphql.Field, error) {
+	v, ok := s.data[c.Name]
+	if !ok {
+		return nil, errFieldNotFound
+	}
+	return &field{value: v}, nil
+}
+
+func (s *selection) Mutate(c *graphql.Context) (graphql.Field, error) {
+	return nil, errNotImplemented
+}
