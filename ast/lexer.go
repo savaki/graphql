1eb9961bd6ac55ae608717c89e196e9b67c187d2
savaki <matt.ho@gmail.com>
2015-07-21 08:38:52 -0700

    - separated generic lexer stuff into its own file

--- ast/lexer.go
+++ ast/lexer.go
@@ -1,1 +1,152 @@
 package ast
+
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
+
+type Pos int
+
+// item represents a token or text string returned from the scanner.
+type item struct {
+	typ itemType // The type of this item.
+	pos Pos      // The starting position, in bytes, of this item in the input string.
+	end Pos      // the length of this item
+	val string   // The value of this item.
+}
+
+//go:generate stringer -type=itemType
+// itemType identifies the type of lex items.
+type itemType int
+
+const eof = -1
+
+// stateFn represents the state of the scanner as a function that returns the next state.
+type stateFn func(*lexer) stateFn
+
+// lexer holds the state of the scanner.
+type lexer struct {
+	name    string      // the name of the input; used only for error reports
+	input   string      // the string being scanned
+	state   stateFn     // the next lexing function to enter
+	pos     Pos         // current position in the input
+	start   Pos         // start position of this item
+	width   Pos         // width of last rune read from input
+	lastPos Pos         // position of most recent item returned by nextItem
+	items   chan item   // channel of scanned items
+	depth   int         // selector depth
+	token   [2]itemType // two-token look behind for parser.
+}
+
+// next returns the next rune in the input.
+func (l *lexer) next() rune {
+	if int(l.pos) >= len(l.input) {
+		l.width = 0
+		return eof
+	}
+	r, w := utf8.DecodeRuneInString(l.input[l.pos:])
+	l.width = Pos(w)
+	l.pos += l.width
+	return r
+}
+
+// peek returns but does not consume the next rune in the input.
+func (l *lexer) peek() rune {
+	r := l.next()
+	l.backup()
+	return r
+}
+
+// backup steps back one rune. Can only be called once per call of next.
+func (l *lexer) backup() {
+	l.pos -= l.width
+}
+
+// emit passes an item back to the client.
+func (l *lexer) emit(t itemType) {
+	l.items <- item{t, l.start, l.pos, l.input[l.start:l.pos]}
+	l.start = l.pos
+
+	// two token look behind
+	l.token[1] = l.token[0]
+	l.token[0] = t
+}
+
+// ignore skips over the pending input before this point.
+func (l *lexer) ignore() {
+	l.start = l.pos
+}
+
+// accept consumes the next rune if it's from the valid set.
+func (l *lexer) accept(valid string) bool {
+	if strings.IndexRune(valid, l.next()) >= 0 {
+		return true
+	}
+	l.backup()
+	return false
+}
+
+func (l *lexer) acceptOrdered(valid string) bool {
+	for index, r := range valid {
+		if l.next() != r {
+			for i := 0; i <= index; i++ {
+				l.backup()
+			}
+			return false
+		}
+	}
+	return true
+}
+
+// acceptRun consumes a run of runes from the valid set.
+func (l *lexer) acceptRun(valid string) int {
+	count := 0
+	for strings.IndexRune(valid, l.next()) >= 0 {
+		count++
+	}
+	l.backup()
+	return count
+}
+
+// acceptFn consumes a run of runes matched by the valid validFn; returns length of run
+func (l *lexer) acceptFn(validFn func(rune) bool) int {
+	count := 0
+	for validFn(l.next()) {
+		count++
+	}
+	l.backup()
+	return count
+}
+
+// lineNumber reports which line we're on, based on the position of
+// the previous item returned by nextItem. Doing it this way
+// means we don't have to worry about peek double counting.
+func (l *lexer) lineNumber() int {
+	return 1 + strings.Count(l.input[:l.lastPos], "\n")
+}
+
+// errorf returns an error token and terminates the scan by passing
+// back a nil pointer that will be the next state, terminating l.nextItem.
+func (l *lexer) errorf(format string, args ...interface{}) stateFn {
+	l.items <- item{itemError, l.start, 0, fmt.Sprintf(format, args...)}
+	return nil
+}
+
+// nextItem returns the next item from the input.
+func (l *lexer) nextItem() item {
+	item := <-l.items
+	l.lastPos = item.pos
+	return item
+}
+
+// lex creates a new scanner for the input string.
+func lex(name, input string) *lexer {
+	l := &lexer{
+		name:  name,
+		input: input,
+		items: make(chan item),
+	}
+	go l.run()
+	return l
+}
